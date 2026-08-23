fb8bf06cb45fdd5588b6f892ac7b0cfb437bb607
sameersd-real <subhanisd1977@gmail.com>
2026-08-23 09:13:35 +0530

    updated alarm container to make alarms based on list

--- react-website/client/src/components/AlarmContainer.jsx
+++ react-website/client/src/components/AlarmContainer.jsx
@@ -2,11 +2,12 @@ import Alarm from "./Alarm";
 import "./AlarmContainer.css";
 
 export default function AlarmContainer() {
+  const alarms = [1, 2, 3];
   return (
     <div className="alarm-container">
-      <Alarm />
-      <Alarm />
-      <Alarm />
+      {alarms.map((item) => (
+        <Alarm key={item} />
+      ))}
     </div>
   );
 }
